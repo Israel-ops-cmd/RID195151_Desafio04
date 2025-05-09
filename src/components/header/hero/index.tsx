24c7e8394d43a5d220509a7273b6683e01cf516c
Israel-ops-cmd <israellipe2020@gmail.com>
2025-05-09 13:59:31 -0300

    Feat: More Animations

--- src/components/header/hero/index.tsx
+++ src/components/header/hero/index.tsx
@@ -1,20 +1,23 @@
 import React from "react"
 import { Container, Text, CodeImage } from "./styles"
+import ScrollFadeIn from "../../animations/scrollFadeIn"
 
 const Hero: React.FC = () => {
     return (
-        <Container id="tecnologias">
-            <Text>
-                <h1>Israel Araújo</h1>
-                <p>Sou um desenvolvedor front-end especializado em transformar ideias em interfaces funcionais, acessíveis e com código de qualidade.</p>
-                <a href="#sobre">
-                    <button>Saber mais</button>
-                </a>
-            </Text>
-            <CodeImage>
-                <img src="/image.webp" alt="" />
-            </CodeImage>
-        </Container>
+        <ScrollFadeIn>
+            <Container id="tecnologias">
+                <Text>
+                    <h1>Israel Araújo</h1>
+                    <p>Sou um desenvolvedor front-end especializado em transformar ideias em interfaces funcionais, acessíveis e com código de qualidade.</p>
+                    <a href="#sobre">
+                        <button>Saber mais</button>
+                    </a>
+                </Text>
+                <CodeImage>
+                    <img src="/image.webp" alt="" />
+                </CodeImage>
+            </Container>
+        </ScrollFadeIn>
     )
 }
 
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,24 +2,27 @@ import React from "react"
 import { Container, Nav, NavLink, SocialIcons } from "./styles"
 import githubIcon from "../../assets/githubIcon.svg"
 import linkedinIcon from "../../assets/linkedinIcon.svg"
+import ScrollFadeIn from "../animations/scrollFadeIn"
 
 const Header: React.FC = () => {
     return (
-        <Container>
-            <Nav>
-                <NavLink href="#projetos">Projetos</NavLink>
-                <NavLink href="#tecnologias">Tecnologias</NavLink>
-                <NavLink href="#sobre">Sobre mim</NavLink>
-            </Nav>
-            <SocialIcons>
-                <a href="https://github.com/Israel-ops-cmd" target="_blank">
-                    <img src={githubIcon} alt="" />
-                </a>
-                <a href="https://www.linkedin.com/in/israelbrands/" target="_blank">
-                    <img src={linkedinIcon} alt="" />
-                </a>
-            </SocialIcons>
-        </Container>
+        <ScrollFadeIn>
+            <Container>
+                <Nav>
+                    <NavLink href="#projetos">Projetos</NavLink>
+                    <NavLink href="#tecnologias">Tecnologias</NavLink>
+                    <NavLink href="#sobre">Sobre mim</NavLink>
+                </Nav>
+                <SocialIcons>
+                    <a href="https://github.com/Israel-ops-cmd" target="_blank">
+                        <img src={githubIcon} alt="" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/israelbrands/" target="_blank">
+                        <img src={linkedinIcon} alt="" />
+                    </a>
+                </SocialIcons>
+            </Container>
+        </ScrollFadeIn>
     )
 }
 
